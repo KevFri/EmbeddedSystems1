aa70734b2352d24d9fbf7ad4f7118a83cab412fc
Kevin Fritz <Kevin.F1@gmx.net>
2017-05-02 11:46:28 +0200

    changed oscillator configuration

--- edaPIC33Oscillator.h
+++ edaPIC33Oscillator.h
@@ -11,7 +11,33 @@
 #include<xc.h>
 #include<stdint.h>
 
-// Select Internal FRC at POR
+//select external oscillator with 8mhz
+_FOSCSEL(FNOSC_PRIPLL & IESO_OFF); //Initial Oscillator: Primary Oscillator (XT, HS, EC) with PLL
+_FOSC(FCKSM_CSECMD & OSCIOFNC_OFF & POSCMD_XT);  //HS Crystal Oscillator Mode
+
+
+
+void configOscillator()
+{
+    // Configure PLL prescaler, PLL postscaler, PLL divisor
+    PLLFBD=68; // M=48
+    CLKDIVbits.PLLPOST=0; // N2=2
+    CLKDIVbits.PLLPRE=0; // N1=2
+    // Initiate Clock Switch to Primary Oscillator with PLL (NOSC=0b011)
+    __builtin_write_OSCCONH(0x03);
+    __builtin_write_OSCCONL(OSCCON | 0x01);
+    // Wait for Clock switch to occur
+    while (OSCCONbits.COSC!= 0b011);
+    // Wait for PLL to lock
+    while (OSCCONbits.LOCK!= 1);
+}
+
+
+
+
+/* configuration for internal Oscillator
+ 
+ // Select Internal FRC at POR
 _FOSCSEL(FNOSC_FRC & IESO_OFF);
 // Enable Clock Switching and Configure Primary Oscillator in XT mode
 _FOSC(FCKSM_CSECMD & OSCIOFNC_OFF & POSCMD_NONE);
@@ -21,9 +47,9 @@ _FOSC(FCKSM_CSECMD & OSCIOFNC_OFF & POSCMD_NONE);
 void configOscillator()
 {   //configuration for 120MHz:
     // Configure PLL prescaler, PLL postscaler, PLL divisor
-    PLLFBD=63; // M=65
+    PLLFBD=74; // 120Mhz:63 / 140Mhz:74
     CLKDIVbits.PLLPOST=0; // N2=2
-    CLKDIVbits.PLLPRE=1; // N1=3
+    CLKDIVbits.PLLPRE=0; // N1=2
     
     // Initiate Clock Switch to FRC oscillator with PLL (NOSC=0b001)
     __builtin_write_OSCCONH(0x01);
@@ -34,5 +60,10 @@ void configOscillator()
     // Wait for PLL to lock
     while (OSCCONbits.LOCK!= 1);
 }
+ 
+ 
+ 
+ 
+ */
 
 #endif
