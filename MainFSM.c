9937ee2a6519c2bf080b5a47a95b9a3b2602d7b8
Kevin Fritz <Kevin.F1@gmx.net>
2017-05-16 08:39:57 +0200

    finite state machine

---
+++ MainFSM.c
@@ -0,0 +1,113 @@
+/** 
+ * @file   MainVorlage.c
+ * @author Kevin Fritz
+ *
+ * @date 26.04.2075, 18:14
+ */
+
+
+
+// Check for Project Settings
+#ifndef __dsPIC33EP512MU810__
+    #error "Wrong Controller"
+#endif
+
+/* ***********************
+ * Includes
+ * ***********************
+ */
+
+#include <xc.h>         //Include appropriate controller specific headers
+#include <stdint.h>     //Standard typedefs
+#include "edaPIC33Hardware.h"
+#include "edaPIC33SystemTime.h"
+#include "edaPIC33Oscillator.h"
+#include "edaPIC33BlinkLed.h"
+
+/* ***********************
+ * Configuration Bits
+ * ***********************
+ */
+
+/* ***********************
+ * Defines
+ * ***********************
+ */
+
+/* Substitute for stdlib.h */
+#define	EXIT_SUCCESS	0
+#define	EXIT_FAILURE	1
+
+
+/* ***********************
+ * Prototypes
+ * ***********************
+ */
+
+/* ***********************
+ * Definitions
+ * ***********************
+ */
+
+
+/* ***********************
+ * Main
+ * ***********************
+ */
+//_FWDT(FWDTEN_OFF); set watchdog (enable) off
+
+/**
+ * Main Function
+ */
+int main() {
+
+    
+    
+    configOscillator();
+    
+    //set LED pinmodes
+    pinMode(LED0,OUTPUT);
+    pinMode(LED1,OUTPUT);
+    pinMode(LED2,OUTPUT);
+    pinMode(LED3,OUTPUT);
+    
+    //set switch pinmodes
+    pinMode(SW0, INPUT_PULLUP);
+    pinMode(SW1, INPUT_PULLUP);
+    pinMode(SW2, INPUT_PULLUP);
+    pinMode(SW3, INPUT_PULLUP);
+    
+    //config timer 1 for getSystemTimeMillis();)
+    configSystemTimeMillis();
+    
+    
+    /* Endless Loop */
+    while(1){
+        static uint32_t ui32Time= 0;
+        //LATBbits.LATB8=1; //toggle LED0
+        /*
+        blinkLed(LED0, measureToggleTimeSW(SW0));
+        blinkLed(LED1, measureToggleTimeSW(SW1));
+        blinkLed(LED2, measureToggleTimeSW(SW2));
+        blinkLed(LED3, measureToggleTimeSW(SW3));*/
+        
+        
+        blinkLed(LED0, 125);
+        blinkLed(LED1, 250);
+        blinkLed(LED2, 500);
+        blinkLed(LED3, 1000);
+        
+        
+        ui32Time++; //increase ms counter
+        //LATBbits.LATB8=0; //toggle LED to measure working time of loop
+        
+        while(getSystemTimeMillis() < ui32Time) //wait rest of 1ms
+        {
+            ClrWdt();   //clear watchdog timer
+        }
+            
+    }//while
+    return (EXIT_SUCCESS);  //never reached
+} //main()
+
+
